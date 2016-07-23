ff03683d4e0717f4570c79907b1981ef2b83ec2d
franzparks <franzparks@gmail.com>
2016-07-23 10:18:58 -0700

    configure ui router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,15 @@
  * Main module of the application.
  */
 angular
-  .module('corporateDashBoardApp', []);
+  .module('corporateDashBoardApp', ['ui.router'])
+
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  	$urlRouterProvider.otherwise('/');
+
+  	$stateProvider
+  	.state('home', {
+  		url : '/',
+  		templateUrl: 'views/main.html'
+  	});
+
+}]);
