986376b1b0991516da284393a03e763e65e9bbce
franzparks <franzparks@gmail.com>
2016-07-23 11:44:34 -0700

    add controllers to route config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,23 +12,28 @@ angular
   .module('corporateDashBoardApp', ['ui.router'])
 
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
   	$urlRouterProvider.otherwise('/');
 
   	$stateProvider
 
   	.state('keyMetrics', {
   		url : '/',
-  		templateUrl: 'views/keyMetrics.html'
+  		templateUrl: 'views/keyMetrics.html',
+  		controller : 'KeyMetricsCtrl as keyMetrics'
   	})
 
   	.state('geo', {
   		url : '/geo',
-  		templateUrl: 'views/geo.html'
+  		templateUrl: 'views/geo.html',
+  		controller : 'GeoCtrl as geo'
+
   	})
 
   	.state('dataView', {
   		url : '/data-view',
-  		templateUrl: 'views/dataView.html'
+  		templateUrl: 'views/dataView.html',
+  		controller : 'dataViewCtrl as dataView'
   	});
 
 }]);
