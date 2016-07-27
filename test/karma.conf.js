8bbc1383f1c9ec9614617ae607f05c77eedad601
franzparks <franzparks@gmail.com>
2016-07-26 17:05:51 -0700

    install table

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/fusioncharts/fusioncharts.js',
+      'bower_components/ng-table/dist/ng-table.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
