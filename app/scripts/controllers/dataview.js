3aa9287a5295160ccc4c597658a88325099fc6ed
franzparks <franzparks@gmail.com>
2016-07-27 04:12:53 -0700

    update table and filter

--- app/scripts/controllers/dataview.js
+++ app/scripts/controllers/dataview.js
@@ -11,12 +11,12 @@ angular.module('corporateDashBoardApp')
   .controller('DataViewCtrl',['$scope','$filter', 'NgTableParams', function ($scope,$filter, NgTableParams) { //$filter, ngTableParams
   	//var users = [];
 
-    $scope.users = [{"id":1,"first_name":"Philip","last_name":"Kim","email":"pkim0@mediafire.com","country":"Indonesia","ip_address":"29.107.35.8"},
+    /*$scope.users = [{"id":1,"first_name":"Philip","last_name":"Kim","email":"pkim0@mediafire.com","country":"Indonesia","ip_address":"29.107.35.8"},
         {"id":2,"first_name":"Judith","last_name":"Austin","email":"jaustin1@mapquest.com","country":"China","ip_address":"173.65.94.30"},
         {"id":3,"first_name":"Julie","last_name":"Wells","email":"jwells2@illinois.edu","country":"Finland","ip_address":"9.100.80.145"},
         {"id":4,"first_name":"Gloria","last_name":"Greene","email":"ggreene3@blogs.com","country":"Indonesia","ip_address":"69.115.85.157"},
         {"id":50,"first_name":"Andrea","last_name":"Greene","email":"agreene4@fda.gov","country":"Russia","ip_address":"128.72.13.52"}];
-    
+    */
     //var data = [];
 
     /*$scope.usersTable = new NgTableParams({
@@ -37,7 +37,7 @@ angular.module('corporateDashBoardApp')
                 }
             });*/
 
-     this.usersTable = function() {
+     /*$scope.usersTable = function() {
         var parameters = {
         	page: 1,
             count: 10
@@ -51,8 +51,12 @@ angular.module('corporateDashBoardApp')
             }
         };
 
-        return new NgTableParams(parameters, settings);
-    };
+        return new NgTableParams({}, settings);
+    };*/
+
+    var self = this;
+	var data = [{name: "Moroni", age: 50} /*,*/];
+	self.tableParams = new NgTableParams({}, { dataset: data});
 
 
 
