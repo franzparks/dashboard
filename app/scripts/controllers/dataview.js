47eeed63552de5ee165470e66bc66645fbf61911
franzparks <franzparks@gmail.com>
2016-07-27 23:01:11 -0700

    fix data call

--- app/scripts/controllers/dataview.js
+++ app/scripts/controllers/dataview.js
@@ -11,17 +11,18 @@ angular.module('corporateDashBoardApp')
   .controller('DataViewCtrl',['$scope','$filter', 'NgTableParams','getDataService',
      function ($scope,$filter, NgTableParams,getDataService) {
 
-    var self = this;
+    //var self = this;
 	$scope.data = [];
 
     getDataService.getDataViewData().then(function(response) {
         // this callback will be called asynchronously
         // when the response is available
         $scope.data = response.data;
+        $scope.tableParams = new NgTableParams({}, { dataset: $scope.data});
         
     });
 
-	self.tableParams = new NgTableParams({}, { dataset: $scope.data});
+	//$scope.tableParams = new NgTableParams({}, { dataset: $scope.data});
 
 
 
