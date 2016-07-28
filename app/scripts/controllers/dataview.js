4f741445c339ff6b1605324c8f62341dd35c49d1
franzparks <franzparks@gmail.com>
2016-07-27 22:51:43 -0700

    use dataview service

--- app/scripts/controllers/dataview.js
+++ app/scripts/controllers/dataview.js
@@ -8,10 +8,11 @@
  * Controller of the corporateDashBoardApp
  */
 angular.module('corporateDashBoardApp')
-  .controller('DataViewCtrl',['$scope','$filter', 'NgTableParams', function ($scope,$filter, NgTableParams) {
+  .controller('DataViewCtrl',['$scope','$filter', 'NgTableParams','getDataService',
+     function ($scope,$filter, NgTableParams,getDataService) {
 
     var self = this;
-	var data = [{name: "Moroni", age: 50} /*,*/];
+	$scope.data = [];
 	self.tableParams = new NgTableParams({}, { dataset: data});
 
 
