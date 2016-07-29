a0da33befa28fa0ade43f9858c4b3a2b071a4710
franzparks <franzparks@gmail.com>
2016-07-29 15:40:46 -0700

    implement polling on data view controller

--- app/scripts/controllers/dataview.js
+++ app/scripts/controllers/dataview.js
@@ -8,8 +8,8 @@
  * Controller of the corporateDashBoardApp
  */
 angular.module('corporateDashBoardApp')
-  .controller('DataViewCtrl',['$scope','$filter', 'NgTableParams','getDataService','Papa',
-     function ($scope,$filter, NgTableParams,getDataService,Papa) {
+  .controller('DataViewCtrl',['$scope','$interval', 'NgTableParams','getDataService','Papa',
+     function ($scope,$interval, NgTableParams,getDataService,Papa) {
 
     //var self = this;
 	$scope.data = [];
@@ -22,31 +22,42 @@ angular.module('corporateDashBoardApp')
         
     //});
 
+    var refreshData = function(){
+        Papa.parse('../data/MOCK_DATA.csv', {
+            download: true,
+            complete: function(results) {
+            //console.log("Finished:", results.data);
+            //customer_name,customer_email,employee_name,submission_time,status,close_time,issue_description
+            $scope.data = results.data.map(function(arr){
+                if(arr[0] !== 'customer_name' && arr[1] !== 'customer_email'
+                    && arr[2] !== 'employee_name' && arr[3] !== 'submission_time' 
+                    && arr[4] !== 'status' && arr[5] !== 'close_time' && arr[6] !== 'issue_description' ){
+                    var user = {};
+                    user.customer_name = arr[0];
+                    user.customer_email = arr[1];
+                    user.employee_name = arr[2];
+                    user.submission_time = arr[3];
+                    user.status = arr[4] ? 'open' : 'closed';
+                    user.close_time = arr[5];
+                    user.issue_description = arr[6]; 
 
-    Papa.parse('../data/MOCK_DATA.csv', {
-        download: true,
-        complete: function(results) {
-        //console.log("Finished:", results.data);
-        //customer_name,customer_email,employee_name,submission_time,status,close_time,issue_description
-        $scope.data = results.data.map(function(arr){
-            if(arr[0] !== 'customer_name' && arr[1] !== 'customer_email'
-                && arr[2] !== 'employee_name' && arr[3] !== 'submission_time' 
-                && arr[4] !== 'status' && arr[5] !== 'close_time' && arr[6] !== 'issue_description' ){
-                var user = {};
-                user.customer_name = arr[0];
-                user.customer_email = arr[1];
-                user.employee_name = arr[2];
-                user.submission_time = arr[3];
-                user.status = arr[4] ? 'open' : 'closed';
-                user.close_time = arr[5];
-                user.issue_description = arr[6]; 
+                    return user;
+                }
+            });
+            $scope.tableParams = new NgTableParams({}, { dataset: $scope.data});
+        }
+       });
+    };
+    
 
-                return user;
-            }
-        });
-        $scope.tableParams = new NgTableParams({}, { dataset: $scope.data});
-    }
-   });
+    var promise = $interval(refreshData, 1000); 
 
+    // Cancel interval on page changes
+    $scope.$on('$destroy', function(){
+        if (angular.isDefined(promise)) {
+            $interval.cancel(promise);
+            promise = undefined;
+        }
+    });      
 
   }]);
