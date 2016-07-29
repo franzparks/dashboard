28edc958537b6af90999000cf23132649fc80f47
franzparks <franzparks@gmail.com>
2016-07-29 14:29:23 -0700

    update dataview controller

--- app/scripts/controllers/dataview.js
+++ app/scripts/controllers/dataview.js
@@ -23,18 +23,23 @@ angular.module('corporateDashBoardApp')
     //});
 
 
-    Papa.parse('../data/dataview.csv', {
+    Papa.parse('../data/MOCK_DATA.csv', {
         download: true,
         complete: function(results) {
         //console.log("Finished:", results.data);
-
+        //customer_name,customer_email,employee_name,submission_time,status,close_time,issue_description
         $scope.data = results.data.map(function(arr){
-            if(arr[0] !== 'name' && arr[1] !== 'priority' && arr[2] !== 'severity' && arr[3] !== 'status' ){
+            if(arr[0] !== 'customer_name' && arr[1] !== 'customer_email'
+                && arr[2] !== 'employee_name' && arr[3] !== 'submission_time' 
+                && arr[4] !== 'status' && arr[5] !== 'close_time' && arr[6] !== 'issue_description' ){
                 var user = {};
-                user.name = arr[0];
-                user.priority = arr[1];
-                user.severity = arr[2];
-                user.status = arr[3];
+                user.customer_name = arr[0];
+                user.customer_email = arr[1];
+                user.employee_name = arr[2];
+                user.submission_time = arr[3];
+                user.status = arr[4];
+                user.close_time = arr[5];
+                user.issue_description = [6]; 
 
                 return user;
             }
