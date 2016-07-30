6b37e4eb3029aeb8f90dd8c4eec0d22e50ba208c
franzparks <franzparks@gmail.com>
2016-07-30 00:18:11 -0700

    clean up

--- app/scripts/controllers/keymetrics.js
+++ app/scripts/controllers/keymetrics.js
@@ -10,14 +10,38 @@
 angular.module('corporateDashBoardApp')
   .controller('KeyMetricsCtrl',['$scope','$interval','getDataService', function ($scope,$interval,getDataService) {
 
+   $scope.myDataSource = {
+        chart: {
+            caption: "Harry's SuperMart",
+            subCaption: "Top 5 stores in last month by revenue",
+            numberPrefix: "$"
+        },
+        data: [{
+            label: "Bakersfield Central",
+            value: "880000"
+        }, {
+            label: "Garden Groove harbour",
+            value: "730000"
+        }, {
+            label: "Los Angeles Topanga",
+            value: "590000"
+        }, {
+            label: "Compton-Rancho Dom",
+            value: "520000"
+        }, {
+            label: "Daly City Serramonte",
+            value: "330000"
+        }]
+    };
 
-  	this.chartWidth = '100%';
-    this.chartHeight = '400';
-    this.dataType = 'json';
-
-	$scope.reportedIssuesData = {};
+	/*$scope.reportedIssuesData = {};
     $scope.openIssuesData = {};
     $scope.payingCustomersData = {};
+
+    $scope.chartoptions = {
+        "containerBackgroundColor": "#FF0000",
+        "containerBackgroundOpacity": "0"
+    };
 
     var refreshData = function(){
 
@@ -93,6 +117,6 @@ angular.module('corporateDashBoardApp')
             $interval.cancel(promise);
             promise = undefined;
         }
-    });     
+    }); */    
 
   }]);
